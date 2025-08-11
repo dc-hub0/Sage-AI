3c04c0436639f057421d677a7e9ffa68c0e7752b
Deepak Chauhan <deepakchauhan7721@gmail.com>
2025-08-11 12:22:35 +0530

    New

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -5,5 +5,5 @@ import { inngest } from "@/lib/inngest/client";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
-//   functions: [generateIndustryInsights],
+//   functions: [generateIndustryInsights]
 });
