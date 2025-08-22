7adfc8548ad7633f2ea249cebe773a494f0a8ba8
Deepak Chauhan <deepakchauhan7721@gmail.com>
2025-08-22 11:01:57 +0530

    Adding Industry Insights and Dashboard

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,17 +1,22 @@
+import { getIndustryInsights } from "@/actions/dashboard";
+import DashboardView from "./_component/dashboard-view";
 import { getUserOnboardingStatus } from "@/actions/user";
 import { redirect } from "next/navigation";
 
-const IndustryInsightsPage = async () => {
+export default async function DashboardPage() {
+  const { isOnboarded } = await getUserOnboardingStatus();
 
+  // If not onboarded, redirect to onboarding page
+  // Skip this check if already on the onboarding page
+  if (!isOnboarded) {
+    redirect("/onboarding");
+  }
 
-    const { isOnboarded } = await getUserOnboardingStatus();
-  
-    if (!isOnboarded) {
-      redirect("/onboarding");
-    }
+  const insights = await getIndustryInsights();
+
   return (
-    <div>IndustryInsightsPage</div>
-  )
+    <div className="container mx-auto">
+      <DashboardView insights={insights} />
+    </div>
+  );
 }
-
-export default IndustryInsightsPage
--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
-// import { Toaster } from "sonner";
+import { Toaster } from "sonner";
 import Header from "@/components/header";
 import { ThemeProvider } from "@/components/theme-provider";
 import { dark } from "@clerk/themes";
@@ -33,7 +33,7 @@ export default function RootLayout({ children }) {
           >
             <Header />
             <main className="min-h-screen">{children}</main>
-            {/* <Toaster richColors /> */}
+            <Toaster richColors />
 
             <footer className="bg-muted/50 py-12">
               <div className="container mx-auto px-4 text-center text-gray-200">
