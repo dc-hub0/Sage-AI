45fe232af07c1843959ec1b74ceb7fe4e7ffe557
Deepak Chauhan <deepakchauhan7721@gmail.com>
2025-09-09 12:44:02 +0530

    Ready For Deployment

--- components/hero.jsx
+++ components/hero.jsx
@@ -46,7 +46,7 @@ const HeroSection = () => {
               Get Started
             </Button>
           </Link>
-          <Link href="/demo">
+          <Link href="/">
             <Button size="lg" variant="outline" className="px-8">
               Watch Demo
             </Button>
